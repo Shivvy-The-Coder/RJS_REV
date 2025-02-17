059a4ea27840e4b7c1ee7832bc4b48bfafa562c6
Shivam kumar das <krshivam455@gmail.com>
2025-02-17 18:00:41 +0530

    files has been added

---
+++ Basics/01Datatypes.js
@@ -0,0 +1,14 @@
+"use strict";
+// this will code the entire code as newer version of javascript
+  let score = "33"
+  console.log(typeof score);
+
+  let valueInNUmber = Number (score);
+  console.log(typeof valueInNUmber)
+
+  var num = "33abc"
+  console.log(typeof num);
+
+  let Num = Number (num)
+  console.log(typeof Num);
+  
