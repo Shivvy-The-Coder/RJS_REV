24608c52debe93a88adea2c91d5aa4476e8addd3
Shivam <139750193+Shivvy-The-Coder@users.noreply.github.com>
2025-06-01 14:48:58 +0530

    using global context

--- React Rev/Day3/src/main.jsx
+++ React Rev/Day3/src/main.jsx
@@ -1,6 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import GlobalState from './context/index.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <App />
+    <GlobalState>
+        <App />
+    </GlobalState>
+    
 )
